fc96c899c6e438a65ba64b129fc3562c24bd3152
jonathanhenriques <silva.henriquejonas@gmail.com>
2022-05-11 16:21:09 -0300

    Tratamento de erro Exe 4

--- poo1105/exer4/Funcionario.java
+++ poo1105/exer4/Funcionario.java
@@ -7,41 +7,47 @@ public class Funcionario {
 	private String nome;
 	private String cargo;
 	private double salario;
-	
+
 	static Scanner scan = new Scanner(System.in);
-	
+
 	public Funcionario(String nome, String cargo, double salario) {
 		this.nome = nome;
 		this.cargo = cargo;
 		this.salario = salario;
 	}
-	
+
 	public static Funcionario cadastraFuncionario() {
-		
+
 		System.out.println("Olá. Por favor, informe nome do funcionário: ");
 		String nome = scan.nextLine();
 		System.out.println("Informe cargo do funcionário: ");
 		String cargo = scan.nextLine();
-		System.out.println("Informe salário do funcionário: ");
-		double salario = scan.nextDouble();
-		
+
+		double salario = 0.0;
+		try {
+			System.out.println("Informe salário do funcionário: ");
+			salario = scan.nextDouble();
+		} catch (Exception e) {
+			System.err.println("Insira somente números!");
+		}
+
 		Funcionario funcionario = new Funcionario(nome, cargo, salario);
-		
+
 		return funcionario;
 	}
-	
+
 	public void informacoesFuncionario() {
 		System.out.println(this.toString());
 	}
-	
+
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("\n***INFORMAÇÕES DO FUNCIONÁRIO***");
-		sb.append("\nNome do funcionário: " + nome);
-		sb.append("\nCargo do funcionário: " + cargo);
-		sb.append("\nSalário do funcionário: " + salario);
+		sb.append("\nNome: " + nome);
+		sb.append("\nCargo: " + cargo);
+		sb.append("\nSalário: R$" + salario);
 		return sb.toString();
 	}
-	
+
 }
